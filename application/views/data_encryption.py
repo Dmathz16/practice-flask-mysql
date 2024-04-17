cf94d12ef0302807f099164f5d7a21880939e74e
Rogincel Demata <roginceldemata@gmail.com>
2024-04-17 18:19:10 +0800

    v 1.10
    Data encryption done!

--- application/views/data_encryption.py
+++ application/views/data_encryption.py
@@ -2,6 +2,9 @@ from flask import abort, Blueprint, render_template, request, flash, session, g,
 from flask_bcrypt import check_password_hash
 import flask
 
+from cryptography.fernet import Fernet
+
+
 from sqlalchemy.exc import IntegrityError
 
 from application import db, bcrypt
@@ -12,7 +15,46 @@ data_encryption = Blueprint('data_encryption', __name__)
 
 title = 'Data Encryption'
 
-@data_encryption.route('/data-encryption/')
+# key = Fernet.generate_key()
+key = b'xkN6Y1kflm0mdRtNTHvJ_FjiBYMVIQM84jMXsYX4huk='
+f = Fernet(key)
+
+def encrypt_data(data):
+    encrypted_data = f.encrypt(data.encode('utf-8'))
+    return encrypted_data.decode('utf-8')
+
+def decrypt_data(data):
+    decrypted_data = f.decrypt(data.encode('utf-8'))
+    return decrypted_data.decode('utf-8')
+
+@data_encryption.route('/data-encryption/', methods=['GET', 'POST'])
 def index():
     data = { "title": title }
+    
+    
+    forEncryption   = ''
+    forDecryption   = ''
+    encrypted       = ''
+    decrypted       = ''
+    
+    if request.method == 'POST':
+        
+        requests = {
+            "username": request_input("username"), 
+            "password": request_input("password"), 
+        }
+        
+        forEncryption = request_input("forEncryption")
+        if forEncryption != '':
+            encrypted = encrypt_data(forEncryption)
+        
+        forDecryption = request_input("forDecryption")
+        if forDecryption != '':
+            decrypted = decrypt_data(forDecryption)
+    
+    data['forEncryption'] = forEncryption
+    data['forDecryption'] = forDecryption
+    data['encrypted'] = encrypted
+    data['decrypted'] = decrypted
+    
     return render_template('data_encryption/index.html', data=data)
